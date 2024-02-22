77fd9b2360ccc0df580cd698447f0c5306baf856
Matt Austin <matt@tunabelly.com>
2024-02-21 19:38:19 -0500

    Removed unused code for macOS 10.9 Mavericks and some commented out code + aligned code.

--- PFAboutWindow/PFAboutWindowController.m
+++ PFAboutWindow/PFAboutWindowController.m
@@ -55,91 +55,75 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
-	self.windowState = 0;
-	self.infoView.layer.cornerRadius = 10.0;
-	self.window.backgroundColor = [NSColor windowBackgroundColor];
+    self.windowState = 0;
+    self.infoView.layer.cornerRadius = 10.0;
+    self.window.backgroundColor = [NSColor windowBackgroundColor];
     [self.window setHasShadow:self.windowShouldHaveShadow];
     // Change highlight of the `visitWebsiteButton` when it's clicked. Otherwise, the button will have a highlight around it which isn't visually pleasing.
-       [self.visitWebsiteButton.cell setHighlightsBy:NSContentsCellMask];
-   
+    [self.visitWebsiteButton.cell setHighlightsBy:NSContentsCellMask];
+    
     // Load variables
     NSDictionary *bundleDict = [[NSBundle mainBundle] infoDictionary];
     
     // Set app name
-    if(!self.appName) {
+    if (!self.appName) {
         self.appName = [bundleDict objectForKey:@"CFBundleName"];
     }
     
     // Set app version
-    if(!self.appVersion) {
+    if (!self.appVersion) {
         NSString *version = [bundleDict objectForKey:@"CFBundleVersion"];
         NSString *shortVersion = [bundleDict objectForKey:@"CFBundleShortVersionString"];
         self.appVersion = [NSString stringWithFormat:NSLocalizedString(@"Version %@ (Build %@)", @"Version %@ (Build %@), displayed in the about window"), shortVersion, version];
     }
-	
+    
     // Set copyright
-    if(!self.appCopyright) {
-        
-        if (floor(NSAppKitVersionNumber) <= NSAppKitVersionNumber10_9){
-            //On OS X Mavericks or below
-            
-            //Therefore we need to set properties that are available on OS X Mavericks or below
-            self.appCopyright = [[NSAttributedString alloc] initWithString:[bundleDict objectForKey:@"NSHumanReadableCopyright"] attributes:@{
-                                                                                                                                              NSForegroundColorAttributeName : [NSColor lightGrayColor],//Looks very close to 'tertiaryLabelColor' on OS X Yosemite
-                                                                                                                                              NSFontAttributeName:[NSFont fontWithName:@"HelveticaNeue" size:11]/*/NSParagraphStyleAttributeName  : paragraphStyle*/}];
-            
-        } else{
-            
-            //On OS 10.10 or later. We don't need to do anything special
-            
-            self.appCopyright = [[NSAttributedString alloc] initWithString:[bundleDict objectForKey:@"NSHumanReadableCopyright"] attributes:@{
-                                                                                                                                              NSForegroundColorAttributeName : [NSColor tertiaryLabelColor],
-                                                                                                                                              NSFontAttributeName			: [NSFont fontWithName:@"HelveticaNeue" size:11]/*,
-                                                                                                                                                                                                                             NSParagraphStyleAttributeName  : paragraphStyle*/}];
-        }
-
+    if (!self.appCopyright) {
+        self.appCopyright = [[NSAttributedString alloc] initWithString:[bundleDict objectForKey:@"NSHumanReadableCopyright"] attributes:@{
+            NSForegroundColorAttributeName : [NSColor tertiaryLabelColor],
+            NSFontAttributeName			: [NSFont fontWithName:@"HelveticaNeue" size:11] }];
     }
-	
+    
     // get the default text color for the current UI appearance
     NSDictionary *textColorAttribs = @{NSForegroundColorAttributeName : [NSColor textColor]};
     
-	// Code that can potentially throw an exception
-	
-	// Set credits
-	if(!self.appCredits) {
-		@try {
+    // Code that can potentially throw an exception
+    
+    // Set credits
+    if(!self.appCredits) {
+        @try {
             // make sure the Credits are displayed with the correct text color for the UI mode (light vs dark)
-			NSAttributedString *tempCredits = [[NSAttributedString alloc] initWithPath:[[NSBundle mainBundle] pathForResource:@"Credits" ofType:self.creditsFileExtension] documentAttributes:nil];
+            NSAttributedString *tempCredits = [[NSAttributedString alloc] initWithPath:[[NSBundle mainBundle] pathForResource:@"Credits" ofType:self.creditsFileExtension] documentAttributes:nil];
             self.appCredits = [[NSMutableAttributedString alloc] initWithAttributedString:tempCredits];
             [self.appCredits addAttributes:textColorAttribs range:NSMakeRange(0, self.appCredits.length)];
-		}
-		@catch (NSException *exception) {
-			// hide the credits button
-			[self.creditsButton setHidden:YES];
-			
-			 NSLog(@"PFAboutWindowController did handle exception: %@",exception);
-		}
-	}
-	
-	// Set EULA
-	if(!self.appEULA) {
-		@try {
+        }
+        @catch (NSException *exception) {
+            // hide the credits button
+            [self.creditsButton setHidden:YES];
+            
+            NSLog(@"PFAboutWindowController did handle exception: %@",exception);
+        }
+    }
+    
+    // Set EULA
+    if(!self.appEULA) {
+        @try {
             // make sure the EULA is displayed with the correct text color for the UI mode (light vs dark)
             NSAttributedString *tempEULA = [[NSAttributedString alloc] initWithPath:[[NSBundle mainBundle] pathForResource:@"EULA" ofType:self.eulaFileExtension] documentAttributes:nil];
             self.appEULA = [[NSMutableAttributedString alloc] initWithAttributedString:tempEULA];
             [self.appEULA addAttributes:textColorAttribs range:NSMakeRange(0, self.appEULA.length)];
         }
-		@catch (NSException *exception) {
-			// hide the eula button
-			[self.EULAButton setHidden:YES];
-			
-			NSLog(@"PFAboutWindowController did handle exception: %@",exception);
-		}
-	}
-
-	[self.textField.textStorage setAttributedString:self.appCopyright];
-	self.creditsButton.title = NSLocalizedString(@"Acknowledgments", @"Caption of the 'Credits' button in the about window");
-	self.EULAButton.title = NSLocalizedString(@"License Agreement", @"Caption of the 'License Agreement' button in the about window");
+        @catch (NSException *exception) {
+            // hide the eula button
+            [self.EULAButton setHidden:YES];
+            
+            NSLog(@"PFAboutWindowController did handle exception: %@",exception);
+        }
+    }
+    
+    [self.textField.textStorage setAttributedString:self.appCopyright];
+    self.creditsButton.title = NSLocalizedString(@"Acknowledgments", @"Caption of the 'Credits' button in the about window");
+    self.EULAButton.title = NSLocalizedString(@"License Agreement", @"Caption of the 'License Agreement' button in the about window");
 }
 
 - (BOOL)windowShouldClose:(id)sender {
